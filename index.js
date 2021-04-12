403e1e88237fe9556f73eab0d563768647c2813b
carl <karol-nowakowski@o2.pl>
2021-04-12 13:33:33 +0200

    Create config and routes file

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const app = express();
-const {port} = require('./config');
+const { port } = require('./config');
+const apiRouter = require('./routes/api');
+
+
+//routes
+
+app.use('/', apiRouter);
 
 
 
-app.get('/', function (request, response) {
-    response.send('Serwer działa');
-})
-app.get('/karol', function (request, response) {
-    response.send('podstrona karol działa');
-})
-
-
-
+//serwer
 app.listen(port, function () {
     console.log('serwer słucha... http://localhost:'+port);
 })
