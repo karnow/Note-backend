441fcd717f802ac1517a7309b1219e9f72e0f768
carl <karol-nowakowski@o2.pl>
2021-04-13 13:26:30 +0200

    Refactoring data Model,actions and router

--- routes/actionsApi/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    homepage (request, response) {
-    response.send('<h1>Serwer działa</h1>');
-    },
-    page (request, response) {
-    response.send('podstrona karol działa');
-}
-}
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const testActions = require('./actionsApi/test');
+const noteActions = require('../actionsApi/notes');
 
 
-router.get('/', testActions.homepage);
-router.get('/karol', testActions.page);
+router.get('/', noteActions.saveNote);
+
 
 
 
