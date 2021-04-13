2dd8a0f2c2658201c79e9bb89368b5763bcea0be
carl <karol-nowakowski@o2.pl>
2021-04-13 12:19:09 +0200

    Data model and data transfer

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/notes-app', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://127.0.0.1:27017/notes-app', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Note = mongoose.model('Note', {
+    title: String,
+    body: String
+});
+
+
+const newNote = new Note({ title: 'tutuł testowy', body: 'tekst naszej notatki' });
+
+newNote.save().then(()=> console.log('notatka została zapisana'));
