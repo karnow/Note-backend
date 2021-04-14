c4e481441bb2063c122555b97927dd84be9024ea
carl <karol-nowakowski@o2.pl>
2021-04-14 12:12:22 +0200

    Create Schema to model

--- db/models/note.js
+++ db/models/note.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
-const Note = mongoose.model('Note', {
-    title: String,
-    body: String
-});
+const NoteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    body: {
+        type: String,
+        require: true,
+    }
+
+})
+
+const Note = mongoose.model('Note', NoteSchema);
 
 module.exports = Note;
